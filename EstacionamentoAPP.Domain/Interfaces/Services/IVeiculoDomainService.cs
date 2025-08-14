5a5c59bd57d960ff342813c0224d07f8c2291f91
Pontesmt <lucaspontesmonteiro2005@gmail.com>
2025-08-13 22:05:19 -0300

    Criação das interfaces para definir os metodos que foram implementados pelos serviços de dominio. E tambem criei uma classe que pode fornecer corpo para todos os metodos abstratos da interface.
    Implementei a interface

---
+++ EstacionamentoAPP.Domain/Interfaces/Services/IVeiculoDomainService.cs
@@ -0,0 +1,16 @@
+﻿using EstacionamentoApp.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoApp.Domain.Service
+{
+    public interface IVeiculoDomainService
+    {
+        CadastroVeiculoResponseDto CadastroVeiculo
+               (CadastroVeiculoRequestDto request);
+
+    }
+}
---
+++ EstacionamentoAPP.Domain/Services/VeiculoDomainService.cs
@@ -0,0 +1,40 @@
+﻿using EstacionamentoApp.Domain.Dtos;
+using EstacionamentoApp.Domain.Validations;
+using EstacionamentoApp.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EstacionamentoApp.Domain.Service
+{
+    public class VeiculoDomainService : IVeiculoDomainService
+    {
+        public CadastroVeiculoResponseDto CadastroVeiculo
+               (CadastroVeiculoRequestDto request)
+        {
+            var veiculo = new Veiculo
+            {
+                Id = Guid.NewGuid(),
+                NomeDono = request.NomeDono,
+                EmailDono = request.EmailDono,
+                Placa = request.Placa,
+                HorarioEntrada = request.HorarioEntrada
+            };
+
+            var validation = new VeiculoValidator();
+            var result = validation.Validate(veiculo);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
+            return null;
+
+        }
+    }
+}
