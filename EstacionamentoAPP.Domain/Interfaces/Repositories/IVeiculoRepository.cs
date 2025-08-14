4b13ad9550a6fdfdda4a2c397cd5685348ff1ae9
Pontesmt <lucaspontesmonteiro2005@gmail.com>
2025-08-13 21:03:25 -0300

    Update de eventos

---
+++ EstacionamentoAPP.Domain/Interfaces/Repositories/IVeiculoRepository.cs
@@ -0,0 +1,13 @@
+﻿using EstacionamentoApp.Domain.Entities;
+
+namespace EstacionamentoApp.Domain.Interfaces.Repositories
+{
+    public interface IVeiculoRepository
+    {
+        void Insert(Veiculo veiculo);
+
+        Veiculo? Find(string email, string placa);
+
+        bool Exists(string email);
+    }
+}
---
+++ UsuariosApp.Infra.Data/Repositories/VeiculoRepository.cs
@@ -0,0 +1,50 @@
+﻿using EstacionamentoApp.Domain.Entities;
+using EstacionamentoApp.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosApp.Infra.Data.Contexts;
+
+
+namespace EstacionamentoApp.Domain.Interfaces.Repositories
+{
+    public class VeiculoRepository : IVeiculoRepository
+    {
+
+        public void Insert(Veiculo veiculo)
+        {
+            using (var dataContext = new DataContext())
+            {
+                dataContext.Add(veiculo);
+                dataContext.SaveChanges();
+            }
+        }
+
+        public Veiculo? Find(string email, string placa)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                .Set<Veiculo>()
+                .Where(v => v.EmailDono.Equals(email)
+                               && v.Placa.Equals(placa)) //filtro
+                .FirstOrDefault();
+                //retornar o primeiro registro encontrado vazio
+
+            }
+        }
+
+        public bool Exists(string email)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                .Set<Veiculo>()
+                .Any(v => v.EmailDono.Equals(email)); //verifica se existe algum registro com o email informado
+            }
+        }
+    }
+}
+
