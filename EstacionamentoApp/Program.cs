136de010db62d3e7339300f29eddd9d18d43893a
Pontesmt <lucaspontesmonteiro2005@gmail.com>
2025-08-14 22:50:58 -0300

    configuração das injeçoes de dependencia do projeto.

--- EstacionamentoApp/Program.cs
+++ EstacionamentoApp/Program.cs
@@ -4,9 +4,8 @@ using EstacionamentoApp.Domain.Interface;
 using EstacionamentoApp.Infra.Data.Repositories;
 
 
-
-
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -17,6 +16,14 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configurar as injeções de dependencia do projeto?
+
+builder.Services.AddTransient
+    <IVeiculoDomainService, VeiculoDomainService>();
+
+builder.Services.AddTransient
+    <IVeiculoRepository, VeiculoRepository>();
 
 var app = builder.Build();
 
