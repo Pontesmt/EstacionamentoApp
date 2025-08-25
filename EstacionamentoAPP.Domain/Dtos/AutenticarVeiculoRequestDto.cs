cc6e5130e815266565805f03289364a2225ba8a8
Pontesmt <lucaspontesmonteiro2005@gmail.com>
2025-08-25 17:15:55 -0300

    Criação dos DTos de Autenticação.

---
+++ EstacionamentoAPP.Domain/Dtos/AutenticarVeiculoRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoApp.Domain.Dtos
+{
+    public class AutenticarVeiculoRequestDto
+    {
+        public string Placa { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ EstacionamentoAPP.Domain/Dtos/AutenticarVeiculoResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoApp.Domain.Dtos
+{
+    public class AutenticarVeiculoResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Placa { get; set; }
+        public string NomeDono { get; set; }
+        public string EmailDono { get; set; }
+        public DateTime HoraEntrada { get; set; }
+        public DateTime? HoraSaida { get; set; }
+        public string token { get; set; }
+    }
+}
